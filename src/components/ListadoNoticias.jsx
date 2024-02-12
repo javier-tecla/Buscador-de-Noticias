bba2e3945eece048ca939ede22341b7f910b72a5
javierborjas <cristman11@gmail.com>
2024-02-12 20:25:10 -0300

    Agregando Paginación

--- src/components/ListadoNoticias.jsx
+++ src/components/ListadoNoticias.jsx
@@ -1,5 +1,7 @@
 import Grid from '@mui/material/Grid'
 import Typography  from '@mui/material/Typography'
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 import useNoticias from '../hooks/useNoticias'
 import Noticia from './Noticia'
 
@@ -32,6 +34,21 @@ const ListadoNoticias = () => {
 
             ))}
         </Grid>
+
+        <Stack
+            sx={{
+                marginY: 5
+            }}
+            spacing={2}
+            direction={'row'}
+            justifyContent='center'
+            alignItems='center'
+        >
+            <Pagination 
+                count={10} 
+                color="primary" 
+            />
+        </Stack>
     </>
   )
 }
