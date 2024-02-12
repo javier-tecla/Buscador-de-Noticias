42e8881700d2f5cb0ebc9a5aa3b774153b259262
javierborjas <cristman11@gmail.com>
2024-02-12 18:17:19 -0300

    Creando el Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Container, Grid, Typography } from '@mui/material'
 import Formulario from './components/Formulario'
+import { NoticiasProvider } from './context/NoticiasProvider'
 
 function App() {
   return (
+    <NoticiasProvider>
     <Container>
         <header>
             <Typography align='center' marginY={5} component="h1" variant="h3">
@@ -17,13 +19,12 @@ function App() {
       alignItems="center"
     >
       <Grid item xs={12} md={6} lg={4}>
-
         <Formulario />
-
       </Grid>
     </Grid>
 
     </Container>
+    </NoticiasProvider>
   )
 }
 
